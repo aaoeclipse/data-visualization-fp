104a5b9b17b3927bada6ca39c7f4dc2ffc9517ee
Santiago Paiz <santiago.paiz@gmail.com>
2022-08-24 13:43:38 +0200

    feat: :hammer: script to download the data

---
+++ download_data.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+curl https://data-visualization-fp.s3.amazonaws.com/data/apidht.csv -o data/apidht.csv 
+curl https://data-visualization-fp.s3.amazonaws.com/data/meta-country-api.csv -o data/meta-country-api.csv
+curl https://data-visualization-fp.s3.amazonaws.com/data/metadata-indicator.csv -o data/metadata-indicator.csv
